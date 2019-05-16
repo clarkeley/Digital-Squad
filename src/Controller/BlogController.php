1d3dddc0966251fa1e9c2ba352b3ec28c00e0ab3
kl-marvin <kl.marvin@outlook.com>
2019-05-16 08:18:15 +0200

    feat blog - articles added + front

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -12,8 +12,27 @@ class BlogController extends AbstractController
      */
     public function index()
     {
-        return $this->render('blog.html.twig', [
+        return $this->render('blog/blog.html.twig', [
             'controller_name' => 'BlogController',
         ]);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/blog/article1", name="article_1")
+     */
+    public function article_1()
+    {
+        return $this->render('blog/article_1.html.twig');
+    }
+
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/blog/article2", name="article_2")
+     */
+    public function article_2()
+    {
+        return $this->render('blog/article_2.html.twig');
     }
 }
